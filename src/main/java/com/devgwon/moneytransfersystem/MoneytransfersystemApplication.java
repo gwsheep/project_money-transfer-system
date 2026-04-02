8cdbd471cca85d8b8ae2644ce216beb71ec458d8
jaekw <jae-kwen@daum.net>
2026-04-02 15:06:50 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/devgwon/moneytransfersystem/MoneytransfersystemApplication.java
@@ -0,0 +1,13 @@
+package com.devgwon.moneytransfersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneytransfersystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneytransfersystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devgwon/moneytransfersystem/MoneytransfersystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devgwon.moneytransfersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneytransfersystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
